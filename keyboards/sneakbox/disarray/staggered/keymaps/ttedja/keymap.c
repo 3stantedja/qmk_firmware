91ac709fd1b44f242be1ad6ca02d7a79433f8232
Fyra Tedja <tristan.tedja@me.com>
2022-06-30 23:22:39 -0600

    add keymap author

--- keyboards/sneakbox/disarray/staggered/keymaps/ttedja/keymap.c
+++ keyboards/sneakbox/disarray/staggered/keymaps/ttedja/keymap.c
@@ -1,5 +1,6 @@
 /*
 Copyright 2021 Bryan Ong
+Copyright 2022 Fyra Tedja
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
